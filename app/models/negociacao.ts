1e8d256651a926269393e672584a9bce6e03d440
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-05-16 20:02:04 -0300

    Integração com o formulário

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,27 @@
 export class Negociacao {
-  #data;
-  #quantidade;
-  #valor;
+  private _data;
+  private _quantidade;
+  private _valor;
   
 
   constructor(data, quantidade, valor){
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
   }
 
   //GET 
   get data(){
-    return this.#data;
+    return this.data;
   }
   get quantidade(){
-    return this.#quantidade;
+    return this.quantidade;
   }
   get valor(){
-    return this.#valor;
+    return this.valor;
   }
   get volume(){
-    return this.#quantidade * this.#valor;
+    return this.quantidade * this.valor;
   }
 
   //SET
